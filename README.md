8c3ed93258430361a24aa37384536bdfdbf5e092
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-09-08 14:41:01 -0600

    04. Web API con ASP.Net
    Se agregó proyecto tipo Web API para Net Core con ASP.Net

---
+++ README.md
@@ -0,0 +1,6 @@
+# Universidad Autónoma de Occidente
+## Aplicaciones Web Avanzadas
+
+
+### Web API con ASP.Net
+**Profesor Bidkar Aragón Cárdenas**
---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace _02_asp_net_web_api_02lsc10v
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
