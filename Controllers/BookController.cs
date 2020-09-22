71e0b01cd8f7a796f70ea82825697bfabd326b85
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-09-22 15:28:34 -0600

    08. ASP.NET Web API
    Endpoint para guardar registros
    TODO: endpoints para editar y eliminar registros

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -22,6 +22,13 @@ namespace _02_asp_net_web_api_02lsc10v.Controllers
     // API RESTful
     // CRUD=SQL=HTTP_METHOD
     // Create=Insert=POST Read=Select=GET Update=Update=PUT Delete=Delete=DELETE
+    // METODO	RUTA	PARAMETROS	RESPUESTA
+    // GET	/api/books	-	Arreglo de objetos de libros
+    // GET	/api/books/{id}	-	Objeto de libro
+    // POST	/api/books	Objeto tipo libro	Objeto de libro ya guardado
+    // PUT	/api/books/{id}	Objeto tipo libro	-
+    // DELETE	/api/books/{id}	-	-
+
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Book>>> GetAllBooks()
@@ -46,6 +53,35 @@ namespace _02_asp_net_web_api_02lsc10v.Controllers
         return NotFound();
       }
       return book;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Book>> SaveNewBook(Book newBook)
+    {
+      _db.Books.Add(newBook);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetBookById), new { id = newBook.Id }, newBook);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> SaveChanges(int id, Book book)
+    {
+      if (id != book.Id)
+      {
+        return BadRequest();
+      }
+
+      _db.Books.Find(id).CategoryId = book.CategoryId;
+
+      try {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        // el libro existe
+        // viole alguna restricción de la base
+      }
     }
   }
 }
